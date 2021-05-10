53ee1da99200f0c76906db80b1485f0c995a9edb
Vinayak Pattade <vinayak.pattade@trafigura.com>
2021-05-10 08:11:38 +0530

    template updated

--- src/codechef/practice/template/Runner.java
+++ src/codechef/practice/template/Runner.java
@@ -3,7 +3,6 @@ package codechef.practice.template;
 public class Runner {
 
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
 		Codechef.main(args);
 	}
 
